f79326fdf4450704c0a8b55e4494d09b10fc5d98
Rafael <akafael@users.noreply.github.com>
2024-07-09 22:20:55 -0300

    Add example with for loop

--- src/02_loops/loops.cpp
+++ src/02_loops/loops.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <sstream>
+
+/**
+ * Example index for
+ */
+std::string loopFor()
+{
+    std::stringstream ss;
+
+    for(int i = 10; i > 0 ; i--)
+    {
+        ss << i << " ";
+    }
+
+    return ss.str();
+}
+
+/**
+ * Example For each
+ */
+std::string loopForEach()
+{
+    std::stringstream ss;
+
+    const int list[10] = {0,1,2,3,4,5,6,7,8,9};
+
+    for( int i: list )
+    {
+        ss << i << " ";
+    }
+
+    return ss.str();
+}
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
-
-    for(int i = 0; i < 10 ; i ++)
-    {
-        std::cout << i << " ";
-    }
+    std::cout << "for loop: " << loopFor() << std::endl;
+    std::cout << "for loop each: " << loopForEach() << std::endl;
 }
